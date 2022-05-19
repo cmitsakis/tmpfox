5b6b4ac4cd811b3f3e2d247750ed1a8808161146
Charalampos Mitsakis <c@mitsakis.org>
2022-05-19 15:44:59 +0300

    download an arkenfox version matching the installed firefox version

--- main.go
+++ main.go
@@ -4,18 +4,23 @@
 package main
 
 import (
+	"archive/zip"
 	"context"
 	"crypto/rand"
 	"encoding/base32"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -43,6 +48,8 @@ type options struct {
 	ExtensionNoRec bool
 }
 
+const notSetUserJsURL = "matching arkenfox version"
+
 func main() {
 	runtime.GOMAXPROCS(1)
 	var o options
@@ -51,7 +58,7 @@ func main() {
 	flag.BoolVar(&o.License, "license", false, "Licensing information")
 	flag.StringVar(&o.ProfilesDir, "dir", filepath.Join(os.TempDir(), appName), "Profiles' directory")
 	flag.BoolVar(&o.Keep, "keep", false, "Do not delete profile on exit")
-	flag.StringVar(&o.UserJsURL, "userjs", "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js", "user.js download URL")
+	flag.StringVar(&o.UserJsURL, "userjs", notSetUserJsURL, "user.js download URL. If not set, download an arkenfox version matching the installed firefox version. If set to empty, do not download user.js.")
 	flag.Var(&o.Extensions, "ext", "Extension to install in the profile. Use the slug name of the extension as argument. You can find the slug at the last part of the URL of the extension: https://addons.mozilla.org/en-US/firefox/addon/slug/. You can use this option multiple times to download multiple extensions. Additionally the following recommended extensions are downloaded: uBlock Origin, ClearURLs, Simple Temporary Containers, Bypass Twitter login wall")
 	flag.BoolVar(&o.ExtensionNoRec, "ext-no-rec", false, "Do not download the recommended extensions (uBlock Origin, ClearURLs, Simple Temporary Containers, Bypass Twitter login wall)")
 	flag.Parse()
@@ -130,7 +137,107 @@ func run(o options) error {
 
 		// download user.js file
 		userJsPath := filepath.Join(profileDirPath, "user.js")
-		if o.UserJsURL != "" {
+		if o.UserJsURL == notSetUserJsURL {
+			// if flag -userjs is not set, download an arkenfox version matching the installed firefox version
+
+			// find installed firefox version
+			output, err := exec.CommandContext(ctx, "firefox", "--version").Output()
+			if err != nil {
+				return fmt.Errorf("failed to run command 'firefox --version': %s", err)
+			}
+			r := regexp.MustCompile(`Mozilla Firefox ([0-9]+)\.[0-9]+\.[0-9]+`)
+			matches := r.FindSubmatch(output)
+			if len(matches) < 2 {
+				return fmt.Errorf("regular expression failed to find matches on the output of command 'firefox --version'")
+			}
+			firefoxVersionMajor, err := strconv.Atoi(string(matches[1]))
+			if err != nil {
+				return fmt.Errorf("failed to identify firefox version on the output of command 'firefox --version': %s", err)
+			}
+
+			// query github for all releases of arkenfox
+			tagsJSON, err := openURLHTML(ctx, client, fmt.Sprintf("https://api.github.com/repos/arkenfox/user.js/git/matching-refs/tags/%v.", firefoxVersionMajor))
+			if err != nil {
+				return fmt.Errorf("failed to query github: %s", err)
+			}
+			var tags []githubTag
+			if err := json.Unmarshal(tagsJSON, &tags); err != nil {
+				return fmt.Errorf("failed to unmarshal github response: %s", err)
+			}
+			if len(tags) == 0 {
+				log.Println("no matching arkefox version found. downloading latest.")
+				err = downloadFile(ctx, client, "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js", userJsPath)
+				if err != nil {
+					return fmt.Errorf("failed to download user.js: %s", err)
+				}
+			} else {
+				var choosenTag githubTag
+				if len(tags) == 1 {
+					choosenTag = tags[0]
+				} else {
+					// if multiple matching tags have been found, choose the one with the highest minor version
+					var maxMinor int
+					for _, tag := range tags {
+						major, minor, err := tag.VersionMajorMinor()
+						if err != nil {
+							continue
+						}
+						if major != firefoxVersionMajor {
+							continue
+						}
+						if minor > maxMinor {
+							maxMinor = minor
+							choosenTag = tag
+						}
+					}
+				}
+				versionString, err := choosenTag.VersionString()
+				if err != nil {
+					return fmt.Errorf("invalid tag version")
+				}
+				zipURL := fmt.Sprintf("https://github.com/arkenfox/user.js/archive/refs/tags/%s.zip", versionString)
+				fmt.Println("url", zipURL)
+				zipPath := filepath.Join(profileDirPath, "arkenfox.zip")
+				err = downloadFile(ctx, client, zipURL, zipPath)
+				if err != nil {
+					return fmt.Errorf("failed to download user.js: %s", err)
+				}
+				defer func() {
+					err := os.Remove(zipPath)
+					if err != nil {
+						log.Printf("failed to delete arkenfox zip file at %s - error: %s", zipPath, err)
+						return
+					}
+				}()
+				zipReadCloser, err := zip.OpenReader(zipPath)
+				if err != nil {
+					return fmt.Errorf("failed to open zip: %s", err)
+				}
+				defer zipReadCloser.Close()
+				for _, fileInZip := range zipReadCloser.File {
+					if fileInZip.Name == fmt.Sprintf("user.js-%s/user.js", versionString) {
+						fo, err := fileInZip.Open()
+						if err != nil {
+							return fmt.Errorf("failed to open file in zip: %s", err)
+						}
+						defer fo.Close()
+						userJsFile, err := os.Create(userJsPath)
+						if err != nil {
+							return fmt.Errorf("failed to create file: %s", err)
+						}
+						_, err = io.Copy(userJsFile, fo)
+						if err != nil {
+							return fmt.Errorf("failed to copy file from zip: %s", err)
+						}
+						break
+					}
+				}
+			}
+			// make sure user.js file has been downloaded
+			if _, err := os.Stat(userJsPath); err != nil {
+				return fmt.Errorf("failed to access user.js file: %s", err)
+			}
+		} else if o.UserJsURL != "" {
 			log.Printf("downloading user.js %s --> %s", o.UserJsURL, userJsPath)
 			err = downloadFile(ctx, client, o.UserJsURL, userJsPath)
 			if err != nil {
@@ -200,4 +307,35 @@ func randomProfileName() (string, error) {
 		return "", fmt.Errorf("random number generator failed: %s", err)
 	}
 	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(b[:]), nil
+}
+
+type githubTag struct {
+	Ref string
+	URL string
+}
+
+func (t githubTag) VersionString() (string, error) {
+	r := regexp.MustCompile(`refs/tags/(.+)`)
+	matches := r.FindStringSubmatch(t.Ref)
+	if len(matches) < 2 {
+		return "", fmt.Errorf("failed to parse version of tag")
+	}
+	return matches[1], nil
+}
+
+func (t githubTag) VersionMajorMinor() (int, int, error) {
+	r := regexp.MustCompile(`refs/tags/([0-9]+)\.([0-9]+)`)
+	matches := r.FindStringSubmatch(t.Ref)
+	if len(matches) < 3 {
+		return 0, 0, fmt.Errorf("failed to parse version of tag")
+	}
+	versionMajor, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to parse version")
+	}
+	versionMinor, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to parse version")
+	}
+	return versionMajor, versionMinor, nil
 }
